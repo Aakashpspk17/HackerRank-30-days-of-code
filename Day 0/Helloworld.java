ef24cc26c0608f3bd2fd483493f72ceba055ded3
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-07 00:10:52 +0530

    Update Helloworld.java

--- Day 0/Helloworld.java
+++ Day 0/Helloworld.java
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+import java.util.Scanner;
 
-int main() 
-{
-    /* Declare a variable named 'input_string' to hold our input.*/
-    char input_string[105]; 
-    
-    /* Read a full line of input from stdin and save it to our variable, input_string.*/
-    scanf("%[^\n]", input_string); 
-    
-    /* Print a string literal saying "Hello, World." to stdout using printf.*/
-    printf("Hello, World.\n");
-    
-    /* TO DO: Write a line of code here that prints the contents of input_string to stdout.*/
-    
-	/* Solution starts from here */
-	
-	printf("%s",input_string);
-	
-    /* Solution ends here */
-	return 0;
+public class HelloWorld {
+    public static void main(String[] args) 
+	{
+        /* Create a Scanner object to read input from stdin.*/
+        Scanner scan = new Scanner(System.in);
+
+        /* Read a full line of input from stdin and save it to our variable, inputString.*/
+        String inputString = scan.nextLine();
+
+        /* Close the scanner object, because we've finished reading
+        all of the input from stdin needed for this challenge.*/
+        scan.close();
+
+        /* Print a string literal saying "Hello, World." to stdout.*/
+        System.out.println("Hello, World.");
+
+        /* Print the contents of inputString to stdout.*/
+		
+		/* Solution starts from here */
+        System.out.println(inputString);
+		/* Solution ends here */
+    }
 }
