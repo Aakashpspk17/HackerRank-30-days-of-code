20f1622adadf1a4c5d98a9af648c103569f93bfc
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-07 16:37:40 +0530

    Update Helloworld.java

--- Day 0/Helloworld.java
+++ Day 0/Helloworld.java
@@ -6,7 +6,6 @@ public class HelloWorld {
         /* Create a Scanner object to read input from stdin.*/
         Scanner scan = new Scanner(System.in);
 
-        /* Read a full line of input from stdin and save it to our variable, inputString.*/
         String inputString = scan.nextLine();
 
 
