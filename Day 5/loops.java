50b02d19abe4b5e9996845268ea3194331c7f256
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-08 18:45:43 +0530

    Update loops.java

--- Day 5/loops.java
+++ Day 5/loops.java
@@ -7,7 +7,6 @@ public class multiplicationtable{
    
   
   int num1 = in.nextInt();
-   
   for (int i=0; i< 10; i++){
    System.out.println(num1 + " x " + (i+1) + " = " + 
      (num1 * (i+1)));
