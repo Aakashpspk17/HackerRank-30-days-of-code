9aac3f2da36881c30d128c4814fbb6fcbc219a5d
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-09 12:31:16 +0530

    Update let'sreview.java

--- Day 6/let'sreview.java
+++ Day 6/let'sreview.java
@@ -1,31 +1,43 @@
 import java.io.*;
 import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class Solution {
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        for(int i=0;i<n;i++){
-            String s = sc.nextLine();
-            String r = new StringBuilder(s).reverse().toString();
-            char sArr[] = s.toCharArray();
-            char rArr[] = r.toCharArray();
-            int j = 1;
-            int k = s.length();
-            int flag = 1;
-            while(j < k){
-                if(Math.abs(sArr[j]-sArr[j-1]) != Math.abs(rArr[j]-rArr[j-1])){
-                    System.out.println("Not Funny");
-                    flag = 0;
-                    break;
+        in.nextLine();
+
+        for (int i = 0; i < N; i++) 
+        {
+            String string = in.nextLine();
+            char[] charArray = string.toCharArray();
+
+            for (int j = 0; j < charArray.length; j++) 
+            {
+                if (j % 2 == 0) 
+                {
+                    System.out.print(charArray[j]);
                 }
-                ++j;
             }
-            if(flag == 1){
-            System.out.println("Funny");
+
+            System.out.print(" ");
+
+            for (int j = 0; j < charArray.length; j++) 
+            {
+                if (j % 2 != 0) 
+                {
+                    System.out.print(charArray[j]);
+                }
             }
+
+            System.out.println();
         }
+
+        in.close();
     }
 }
