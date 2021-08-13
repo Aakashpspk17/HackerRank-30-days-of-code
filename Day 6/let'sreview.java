1627e411d2a2e0829f371deaafd607c9e4274e40
AakashEmandi <65613417+Aakashpspk17@users.noreply.github.com>
2021-08-13 11:09:25 +0530

    Update let'sreview.java

--- Day 6/let'sreview.java
+++ Day 6/let'sreview.java
@@ -9,7 +9,6 @@ public class Solution {
     {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
-
         in.nextLine();
 
         for (int i = 0; i < N; i++) 
@@ -26,7 +25,6 @@ public class Solution {
             }
 
             System.out.print(" ");
-
             for (int j = 0; j < charArray.length; j++) 
             {
                 if (j % 2 != 0) 
@@ -34,10 +32,8 @@ public class Solution {
                     System.out.print(charArray[j]);
                 }
             }
-
             System.out.println();
         }
-
         in.close();
     }
 }
